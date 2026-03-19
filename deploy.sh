b2732a2e5a13c80673e38bd7a8a4f191610e2775
Stefan Kupych <stefan@kupy.ch>
2026-03-19 17:37:36 -0400

    Fix deploy

--- deploy.sh
+++ deploy.sh
@@ -7,13 +7,16 @@ REMOTE_DIR="/var/www/jerboa"
 echo ":: building..."
 make build
 
+echo ":: stopping service..."
+ssh "$HOST" "sudo systemctl stop jerboa"
+
 echo ":: deploying binary..."
 scp bin/jerboa "$HOST:$REMOTE_DIR/bin/jerboa"
 
 echo ":: syncing migrations..."
 scp -r migrations/ "$HOST:$REMOTE_DIR/migrations/"
 
-echo ":: restarting service..."
-ssh "$HOST" "sudo systemctl restart jerboa"
+echo ":: starting service..."
+ssh "$HOST" "sudo systemctl start jerboa"
 
 echo ":: done"
